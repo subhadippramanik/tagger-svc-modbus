bd9ff0150e4c14ce55e77843f2b7fde9f720ca16
Subhadip <subhadip.pramanik@siemens.com>
2017-07-28 18:51:15 +0530

    feat: added tagger message

--- src/main/java/gw/bif/tagger/modbus/controller/TaggerModbusController.java
+++ src/main/java/gw/bif/tagger/modbus/controller/TaggerModbusController.java
@@ -1,5 +1,6 @@
 package gw.bif.tagger.modbus.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import gw.bif.tagger.core.message.Tag;
 import gw.bif.tagger.modbus.enricher.ModbusTagEnricher;
 
 @RestController
@@ -15,6 +17,11 @@ public class TaggerModbusController {
 	
 	@Autowired
 	ModbusTagEnricher modbusTagEnricher;
+	
+//	@RequestMapping(value = "/modbus", method = RequestMethod.POST)
+//	public List<Tag> modbus(@RequestBody Tag message) {		
+//		return modbusTagEnricher.enrich(message);
+//	}
 	
 	@RequestMapping(value = "/modbus", method = RequestMethod.POST)
 	public Map<String, String> modbus(@RequestBody Map<String, String> message) {		
