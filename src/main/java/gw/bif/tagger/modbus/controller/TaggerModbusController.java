4b878114e607ae8bcf50cce589f647bf0bfbd9a9
Subhadip Pramanik <subhadip@Subhadips-MacBook-Pro.local>
2017-07-29 14:43:04 +0530

    chore: remove unused code

--- src/main/java/gw/bif/tagger/modbus/controller/TaggerModbusController.java
+++ src/main/java/gw/bif/tagger/modbus/controller/TaggerModbusController.java
@@ -1,6 +1,5 @@
 package gw.bif.tagger.modbus.controller;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import gw.bif.tagger.core.message.Tag;
 import gw.bif.tagger.modbus.enricher.ModbusTagEnricher;
 
 @RestController
@@ -17,12 +15,7 @@ public class TaggerModbusController {
 	
 	@Autowired
 	ModbusTagEnricher modbusTagEnricher;
-	
-//	@RequestMapping(value = "/modbus", method = RequestMethod.POST)
-//	public List<Tag> modbus(@RequestBody Tag message) {		
-//		return modbusTagEnricher.enrich(message);
-//	}
-	
+		
 	@RequestMapping(value = "/modbus", method = RequestMethod.POST)
 	public Map<String, String> modbus(@RequestBody Map<String, String> message) {		
 		return modbusTagEnricher.enrich(message);
