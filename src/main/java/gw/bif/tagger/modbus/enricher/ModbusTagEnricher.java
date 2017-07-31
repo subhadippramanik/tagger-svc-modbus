f8ada7f652a748085ede4bbfe7a10c9468bac296
Subhadip <subhadip.pramanik@siemens.com>
2017-07-31 16:31:29 +0530

    chore: remove camel dependencies

--- src/main/java/gw/bif/tagger/modbus/enricher/ModbusTagEnricher.java
+++ src/main/java/gw/bif/tagger/modbus/enricher/ModbusTagEnricher.java
@@ -12,7 +12,7 @@ Log logger = LogFactory.getLog(this.getClass());
 	
 	public Map<String, String> enrich(Map<String, String> message) {
 		if(message.containsKey("modbus:present-value")) {
-			message.put("modbus:currentValue", message.get("modbus:present-value"));
+			message.put("hs:currentValue", message.get("modbus:present-value"));
 		}
 		logger.info(message);
 		return message;
